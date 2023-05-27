f7df5f81de165cb973c09efa342e6df31c511707
Vaughan Milliman <vaughanm1@protonmail.com>
2023-05-27 13:52:33 -0500

    removed old bimap

--- src/extern/bimap/bimap.d.ts
+++
@@ -1,8 +0,0 @@
-declare class BiMap<K, V> {
-    push(key: K, value: V): void;
-    set(key: K, value: V): void;
-    key(key: K): V | null;
-    val(value: V): K | null;
-    removeKey(key: K): void;
-    removeVal(value: V): void;
-}
