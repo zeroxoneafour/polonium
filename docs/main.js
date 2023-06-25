4abb09eb0eb571ec9adbe4cde8520b7db91f8926
Himprakash Deka <himprakashd@gmail.com>
2023-06-25 16:37:18 +0530

    Add copy button to pre/code snippets

--- docs/main.js
+++ docs/main.js
@@ -12,3 +12,67 @@ include("./scripts/footer.js");
 include("./scripts/navbar.js");
 include("https://cdn.jsdelivr.net/npm/marked/marked.min.js");
 include("https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js");
+
+function getFirstDescendantByClass(parent, className) {
+  var descendants = parent.getElementsByClassName(className);
+  if (descendants.length)
+    return descendants[0];
+  return null;
+}
+
+function getFirstDescendantByTag(parent, tagName) {
+  var descendants = parent.getElementsByTagName(tagName);
+  if (descendants.length)
+    return descendants[0];
+  return null;
+}
+
+function copyToClipboardFromId(textElementId, trigerElement = null) {
+  navigator.clipboard.writeText(document.getElementById(textElementId).innerHTML);
+  trigerElement.innerHTML="done";
+  setTimeout(()=>{
+    trigerElement.innerHTML="content_copy";
+  }, 1000);
+}
+
+function addCopyButton(element, textElementId) {
+  if (getFirstDescendantByClass(element, "button") != null)
+    return;
+  var button = document.createElement('button');
+  button.classList.add('pre-copy-button');
+  button.classList.add('material-symbols-rounded');
+  button.setAttribute('onclick', 'copyToClipboardFromId("' + textElementId + '", this);');
+  button.innerHTML = 'content_copy';
+  element.appendChild(button);
+}
+
+function markdownParse(url, id) {
+  fetch(url).then(function (response) {
+    response.text().then(function (text) {
+
+      var parsedMarkdown = document.createElement('div');
+      parsedMarkdown.innerHTML = marked.parse(text);
+      var preElements = parsedMarkdown.querySelectorAll('pre');
+
+      for (i = 0; i < preElements.length; i++) {
+        var e = preElements[i];
+        var preContainer = document.createElement('div');
+
+        var textElement = getFirstDescendantByTag(e, 'code');
+        if (textElement == null)
+          textElement = e;
+        textElement.setAttribute('id', id + '-pre-' + i);
+
+        pre = document.createElement('pre');
+        pre.innerHTML = e.innerHTML
+        preContainer.appendChild(pre);
+        preContainer.classList.add('pre-container');
+
+        addCopyButton(preContainer, id + '-pre-' + i);
+        e.replaceWith(preContainer);
+      }
+
+      document.getElementById(id).innerHTML = parsedMarkdown.innerHTML;
+    });
+  });
+}
