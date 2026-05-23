4c65531c8ff40b98aa8914bd3f1eb15804d8d557
Vaughan Milliman <vaughan@vaughanm.xyz>
2026-05-23 14:49:43 -0500

    remove src-old

--- src-old/index.ts
+++
@@ -1,8 +0,0 @@
-// index.ts - Entry point from QML to TypeScript
-
-import { QmlApi, QmlObjects } from "./extern/qml";
-import { createController } from "./controller";
-
-export function main(api: QmlApi, qmlObjects: QmlObjects) {
-    const ctrl = createController(api, qmlObjects);
-}
